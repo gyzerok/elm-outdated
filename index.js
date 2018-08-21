9e2c46015e4870138c81d33578cca7128b6ab911
Fedor Nezhivoi <gyzerok@gmail.com>
2018-08-21 16:50:52 +0300

    properly fix for 0.18

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const fs = require('fs');
-const http = require('https');
-const table = require('text-table');
-const clc = require('cli-color');
+const fs = require("fs");
+const http = require("http");
+const table = require("text-table");
+const clc = require("cli-color");
 
-const Elm = require('./elm.js');
-
+const Elm = require("./elm.js");
 
 let elmPackageJson;
 
 try {
-  elmPackageJson = fs.readFileSync('elm-package.json', { encoding: 'utf-8' });
-}
-catch (e) {
-  console.error('There is no elm-package.json in the directory your are running elm-outdated from.');
+  elmPackageJson = fs.readFileSync("elm-package.json", { encoding: "utf-8" });
+} catch (e) {
+  console.error(
+    "There is no elm-package.json in the directory your are running elm-outdated from."
+  );
   process.exit(1);
 }
 
-fetch("https://package.elm-lang.org/all-packages")
+fetch("http://package.elm-lang.org/all-packages?elm-package-version=0.18")
   .then(data => {
     let parsedJson;
 
     try {
       parsedJson = JSON.parse(elmPackageJson);
-    }
-    catch (e) {
-      console.log('Your elm-package.json is corrupted.')
+    } catch (e) {
+      console.log("Your elm-package.json is corrupted.");
       process.exit(1);
     }
 
     const app = Elm.Main.worker({
       elmPackageJson: parsedJson,
-      registry: data,
+      registry: data
     });
 
     app.ports.sendError.subscribe(error => {
@@ -40,22 +39,27 @@ fetch("https://package.elm-lang.org/all-packages")
 
     app.ports.sendReports.subscribe(reports => {
       if (reports.length === 0) {
-        console.log('Everything is up to date!');
-      }
-      else {
+        console.log("Everything is up to date!");
+      } else {
         console.log(
           table(
             [
-              ['Package', 'Current', 'Wanted', 'Latest'].map(h => clc.underline(h)),
-              ...reports
-                .map(([name, report]) => {
-                  return !report
-                    ? [name, 'custom', 'custom', 'custom']
-                    : [name, report.current, clc.green(report.wanted), clc.magenta(report.latest)]
-                })
+              ["Package", "Current", "Wanted", "Latest"].map(h =>
+                clc.underline(h)
+              ),
+              ...reports.map(([name, report]) => {
+                return !report
+                  ? [name, "custom", "custom", "custom"]
+                  : [
+                      name,
+                      report.current,
+                      clc.green(report.wanted),
+                      clc.magenta(report.latest)
+                    ];
+              })
             ],
             {
-              align: ['l', 'r', 'r', 'r'],
+              align: ["l", "r", "r", "r"],
               stringLength: clc.getStrippedLength
             }
           )
@@ -68,20 +72,19 @@ fetch("https://package.elm-lang.org/all-packages")
 function fetch(url) {
   return new Promise((resolve, reject) => {
     http.get(url, res => {
-      res.setEncoding('utf8');
-      let body = '';
-      res.on('data', data => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
         body += data;
       });
-      res.on('end', () => {
+      res.on("end", () => {
         try {
           resolve(JSON.parse(body));
-        }
-        catch (e) {
+        } catch (e) {
           reject(e);
         }
       });
-      res.on('error', reject);
+      res.on("error", reject);
     });
   });
 }
